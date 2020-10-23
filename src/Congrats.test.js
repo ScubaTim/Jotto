26d40e12383cf1c1cf9e2703970fee5217896eb6
Tim Gentry <gentry.tim@gmail.com>
2020-10-23 13:58:28 -0400

    added prop-types

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,12 +2,28 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
+import { findByTestAttr } from '../test/testUtils';
 import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('it renders the Congrats component', () => {
-    const wrapper = shallow(<Congrats />)
-    const congratsComponent = wrapper.find(`[data-test="congrats-component"]`)
-    expect(congratsComponent.length).toBe(1)
+const setup = (props = {}) => {
+    return shallow(<Congrats {...props} />)
+}
+
+test('renders the Congrats component', () => {
+    const wrapper = setup()
+    const component = findByTestAttr(wrapper, 'component-congrats')
+    expect(component.length).toBe(1)
+})
+
+test('renders no text when success prop is false', () => {
+    const wrapper = setup({ success: false })
+    const component = findByTestAttr(wrapper, 'component-congrats')
+    expect(component.text()).toBe('')
+
+})
+
+test('renders text when success prop is true', () => {
+
 })
